31b0b020ef85456ab7c84554cba161d1c88fc372
Mika <mika529.com@gmail.com>
2020-05-24 19:34:21 +0900

    Add the implementation

--- src/main/java/shoohei/bmi/Controller/BmiController.java
+++ src/main/java/shoohei/bmi/Controller/BmiController.java
@@ -8,7 +8,27 @@ public class BmiController {
 
 	@GetMapping("bmi")
 	public String calculateBmi(Integer weight, Double height) {
+		System.out.println("calculateBmi ");
+		double bmiNumber = weight / (height * height);
+		String bmiDescription;
+		if (bmiNumber <= 18.5) {
+			bmiDescription = "Underweight";
+		} else if (bmiNumber <= 25) {
+			bmiDescription = "Normal range";
+		} else if (bmiNumber <= 30) {
+			bmiDescription = "Pre-obese";
+		} else if (bmiNumber <= 35) {
+			bmiDescription = "Obese class@I";
+		} else if (bmiNumber <= 40) {
+			bmiDescription = "Obese class II";
+		} else {
+			bmiDescription = "Obese class III";
+		}
+		String result = "BMI: " + bmiNumber + " (" + bmiDescription + ")";
+		System.out.println(result);
+
+
+		return result;
 		
-		return "TODO 2 : " + weight + height;
 	}
 }
